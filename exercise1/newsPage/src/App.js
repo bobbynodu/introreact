1ceb896e39dc9973f7805fbc5b910250caf9b76d
bobby_nodu@hotmail.com <bobby_nodu@hotmail.com>
2021-10-03 23:44:26 +0300

    exe1

---
+++ exercise1/newsPage/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import MainSection from './components/MainSection';
+import Newsnotification from './components/Newsnotification';
+import SideSection from './components/SideSection';
+import './App.css';
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default App;
---
+++ exercise1/newsPage/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export default function Header() {
+    return (
+        <div className="headercolor">
+            <div className="header">
+
+            <div className="headermain">HELSINGIN SANOMAT</div>
+            <div>Uutiset</div>
+            <div>Lehdet</div>
+            <div className="button">Tilaa</div>
+            <div>Kirjauda</div>
+            <div>Hae</div>
+            <div>Valikko</div>
+            
+            </div>
+        </div>
+    )
+}
